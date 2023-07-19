ad4ed05a23e7d5849b03a509bf05a9470cda038f
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-07-19 00:38:59 -0300

    Criação do card com as configurações de estado

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -17,16 +17,7 @@ export default function Home() {
       <View>
         {/* Sexo */}
         <View>
-          <Card text="Masculino" />
-          {/* <TouchableOpacity className='w-[150px] h-[110px] flex-row items-center justify-center bg-white p-4 shadow rounded-2xl'>
-            <View className='items-center'>
-              <MaleSymbol name="male-symbol" size={56} className='text-slate-800' />
-              <Text className='font-mulish text-sm text-slate-800 mt-[-4px]'>Masculino</Text>
-            </View>
-            <View className='h-full mr-[-20px] mt-[-8px]'>
-              <Selected />
-            </View>
-          </TouchableOpacity> */}
+          <Card text="Masculino" activate={true} />
         </View>
       </View>
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,19 +5,30 @@ import Selected from '../assets/selected.svg';
 import { Foundation } from '@expo/vector-icons';
 import { styled } from "nativewind";
 
-const MaleSymbol = styled(Foundation)
+const Symbol = styled(Foundation)
 
-export function Card() {
+interface Props {
+    text: "Masculino" | "Feminino",
+    activate: boolean,
+}
 
+export function Card({text, activate}: Props) {
+    const colorVariantes = {
+        'active': 'text-slate-800',
+        'disabled': 'text-zinc-400'
+    }
     return (
-        <TouchableOpacity className='w-[150px] h-[110px] flex-row items-center justify-center bg-white p-4 shadow rounded-2xl'>
-            <View className='items-center'>
-              <MaleSymbol name="male-symbol" size={56} className='text-slate-800' />
-              <Text className='font-mulish text-sm text-slate-800 mt-[-4px]'>Masculino</Text>
+        <TouchableOpacity className={`${activate ? 'bg-zinc-100' : 'bg-zinc-300'} w-[150px] h-[110px] flex-row items-center justify-center p-4 shadow rounded-2xl`}>
+            <View className='items-center w-20'>
+              <Symbol name={`${text == 'Masculino' ? 'male' : 'female'}-symbol`} size={56} className={`${colorVariantes[activate ? 'active' : 'disabled']}`} />
+              <Text className={`${colorVariantes[activate ? 'active' : 'disabled']} font-mulish text-sm text-slate-800 mt-[-4px]`}>{text}</Text>
             </View>
+            {activate ? (
             <View className='h-full mr-[-20px] mt-[-8px]'>
-              <Selected />
+                <Selected />
             </View>
+            ) : ''}
+            
           </TouchableOpacity>
     )
 }
